90cd539dd174396d4dc090762ced4a15722244a0
Journo-App <awwoodall@gmail.com>
2015-10-19 09:42:06 -0700

    Removed all datetime code

--- hw.py
+++ hw.py
@@ -1,6 +1,5 @@
+# Everything is running; now to get the HTML to display 10-19-15aw
 from flask import Flask, render_template
-from datetime import datetime
-import datetime
 
 
 app = Flask(__name__)
@@ -26,8 +25,8 @@ def about():
 def contact():
     return render_template('jinja_temp.html',
                            my_string="I'm the contact page",
-                           my_list=[0,1,2,3,4,5],
-                           current_time=datetime.datetime.now())
+                           my_list=[0,1,2,3,4,5])
+                       
                            
 @app.route("/hello/<name>")
 def hi_person(name):
@@ -56,10 +55,5 @@ def jedi(firstname,lastname):
     return html.format(lastname[0:3].title()+firstname[0:2].title())
     
                            
-@app.template_filter()
-def datetimefilter(value, format='%Y/%m/%d %H:%M'):
-    """Convert a datetime to a different format."""
-    return value.strftime(format) 
-
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
